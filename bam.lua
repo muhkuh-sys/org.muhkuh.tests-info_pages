ff8541f71c213131b1dd1e82f86f0b1db3605095
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2023-10-13 15:41:53 +0200

    Increase the project version to v1.2.0 .

--- bam.lua
+++ bam.lua
@@ -20,7 +20,7 @@ local atEnv = require 'mbs'
 --
 
 -- FIXME: The MBS2 system should read this from a setup file.
-atEnv.DEFAULT.atVars.PROJECT_VERSION = { "1", "1", "0" }
+atEnv.DEFAULT.atVars.PROJECT_VERSION = { "1", "2", "0" }
 
 ------------------------------------------------------------------------------
 --
